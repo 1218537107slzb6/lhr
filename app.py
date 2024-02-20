f98f77b7b81a2e845978a6884b3ca4b92b100489
1218537107slzb6 <160474471+1218537107slzb6@users.noreply.github.com>
2024-02-20 14:24:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def index():
   return(render_template("index.html"))
 
+@app.route("/",methods=["GET","POST"])
+def main():
+  name=request.from.get("name")
+  return(render_template("main.html",r=name))
+  
+  
+
+
+
+
 if __name__ == "__main__":
   app.run()
